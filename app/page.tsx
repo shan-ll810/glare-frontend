54568cebced7b625c3145d0f7b0e7f0df4b4557e
sq <sq@sqs-MacBook-Air.local>
2026-04-30 16:01:43 -0700

    top view orientation update

--- app/page.tsx
+++ app/page.tsx
@@ -1651,7 +1651,7 @@ useEffect(() => {
   const windowRightVal = windowLeftVal + windowWidth;
   const windowCenter = windowLeftVal + windowWidth / 2;
 
-  const rotationDeg = -orientationDeg;
+  const rotationDeg = orientationDeg;
 
   const shapes: React.ReactNode[] = [];
 
